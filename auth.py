dc1b519ccc15a0faed7e9110c6db05a59dc1e9d2
Riquelme Ruan <riquelmeruan2302@gmail.com>
2025-10-21 21:28:07 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -5,6 +5,7 @@ import json
 import login
 
 def cadastrar():
+    #Cadastro do cliente/entregador
     valida.limpaTerminal()
     print('\033[1;34mCadastro do usuário\033[m')
     nome = valida.validanome()
@@ -13,9 +14,10 @@ def cadastrar():
     status = valida.validastatus()
      
     if os.path.exists('nome.json'):
+        #Verifica se o arquivo já existe
         with open('nome.json', 'r', encoding='utf-8') as arquivo:
             dados = json.load(arquivo)
-            if email.strip() in dados:
+            if email in dados:
                 print("\033[1;34mEmail já cadastrado no sistema. Estamos encaminhando você para o login...\033[m")
                 sleep(2)
                 login.login()
@@ -29,6 +31,3 @@ def cadastrar():
         json.dump(dados, arquivo, ensure_ascii=False, indent=4)
     sleep(2)
     login.login()
-
-if __name__ == "__main__":
-    cadastrar()
