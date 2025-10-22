5556c042ecea07a2306af94318d7da7a27ffbfe4
Riquelme Ruan <riquelmeruan2302@gmail.com>
2025-10-21 21:30:58 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,6 +6,10 @@ import sys
 import modoadm
 import valida
 import compras
+import bonificacoes
+import rankings
+import entregas
+import json
 def MenuInicial(): #Menu inicial
     opcao = ''
     while opcao != '3':
@@ -32,27 +36,54 @@ def menu_admin(email_logado): #Menu Administrativo
     while True:
         valida.limpaTerminal()
         print("\n--- MENU ADMINISTRATIVO ---")
-        print("1 - Gerenciar Estoque")
-        print("2 - Visualizar Dados")
-        print('3 - Deletar conta')
-        print("4 - Sair")
+        print('1- Estoque')
+        print('2 - Configurações')
+        print('3 - Deletar Entrega')
+        print('4 - Deletar Compra')
+        print("5 - Sair")
 
         opcao = input("Escolha uma opção: ").strip()
 
         if opcao == "1":
             Estoque(email_logado)
         elif opcao == "2":
-            modoadm.MostrarDados(email_logado)
-        elif opcao == "3":
-            print("Configurações do sistema...")
+            ConfiguracoesAdmin(email_logado)
         elif opcao == '3':
-            modoadm.DeletarContaAdmin(email_logado)
-        elif opcao == "4":
+            modoadm.DeletarEntregas(email_logado)
+        elif opcao == '4':
+            modoadm.DeletarCompras(email_logado)
+        elif opcao == "5":
             print("Saindo do modo ADM...")
             sleep(1)
             sys.exit()
         else:
             print("Opção inválida!")
+            sleep(1)
+
+def ConfiguracoesAdmin(email_logado): #Configurações Adm
+    opcao = ''
+    while opcao != '4':
+        valida.limpaTerminal()
+        print('\033[1;34m---Configurações---\033[m')
+        print('1 - Ver dados')
+        print('2- Atualizar Dados')
+        print('3 - Deletar Conta')
+        print('4 - Retornar ao Menu principal')
+        
+        opcao = input('Escolha uma opção: ').strip()
+        if opcao == '1':
+            modoadm.MostrarDados(email_logado)
+        elif opcao == '2':
+            modoadm.AtualizarDados(email_logado)
+        elif opcao == '3':
+            modoadm.DeletarContaAdmin(email_logado)
+        elif opcao == '4':
+            print('Retornando ao Menu principal...')
+            sleep(1)
+            menu_admin(email_logado)
+            break
+        else:
+            print('\033[1;31mA opção é inválida.\033[m')
             sleep(1)
 
 def Estoque(email_logado):# Menu de estoque do adm
@@ -85,7 +116,8 @@ def MenuPrincipal(email_logado): #Menu principal dos clientes
         print('1 - Configurações')
         print('2- Compras')
         print('3 - Rankings')
-        print('4 - Fechar programa')
+        print('4 - Bonificações')
+        print('5 - Fechar programa')
         
         opcao = input('Escolha uma opção: ').strip()
         if opcao == '1':
@@ -93,9 +125,11 @@ def MenuPrincipal(email_logado): #Menu principal dos clientes
         elif opcao == '2':
             Compras(email_logado)
         elif opcao == '3':
-            dados.Rankings(email_logado)
+            rankings.Rankings(email_logado)
         elif opcao == '4':
-            print('Fechando programa...')
+            bonificacoes.HistoricoBonificacoesCliente(email_logado)
+        elif opcao == '5':
+            print('\033[1;36mFechando programa...\033[m')
             sleep(1)
             sys.exit()
         else:
@@ -103,13 +137,14 @@ def MenuPrincipal(email_logado): #Menu principal dos clientes
             sleep(1)
 def MenuPrincipalEntregador(email_logado): #Menu principal dos entregadores
     opcao = ''
-    while opcao != '4':
+    while opcao != '5':
         valida.limpaTerminal()
         print('\033[1;34m--- Menu principal ---\033[m')
         print('1 - Configurações')
         print('2- Entregas')
         print('3 - Rankings')
-        print('4 - Fechar programa')
+        print('4 - Bonificações ')
+        print('5 - Fechar programa')
         
         opcao = input('Escolha uma opção: ').strip()
         if opcao == '1':
@@ -117,15 +152,17 @@ def MenuPrincipalEntregador(email_logado): #Menu principal dos entregadores
         elif opcao == '2':
             MenuEntregas(email_logado)
         elif opcao == '3':
-            dados.RankingEntregadores(email_logado)
+            rankings.RankingEntregadores(email_logado)
         elif opcao == '4':
+            bonificacoes.HistoricoBonificacoesEntregador(email_logado)
+        elif opcao == '5':
             print('Fechando programa...')
             sleep(1)
             sys.exit()
         else:
             print('\033[1;31mA opção é inválida.\033[m')
             sleep(1)
-def MenuEntregas(email_logado):
+def MenuEntregas(email_logado): #Menu de Entregas 
     opcao = ''
     while opcao != '3':
         valida.limpaTerminal()
@@ -136,18 +173,23 @@ def MenuEntregas(email_logado):
         
         opcao = input('Escolha uma opção: ').strip()
         if opcao == '1':
-            compras.RegistraEntrega(email_logado)
+            entregas.RegistraEntrega(email_logado)
         elif opcao == '2':
-            compras.HistoricoEntregas(email_logado)
+            entregas.HistoricoEntregas(email_logado)
         elif opcao == '3':
             print('Retornando ao Menu principal...')
             sleep(1)
-            MenuPrincipal(email_logado)
+            MenuPrincipalEntregador(email_logado)
             break
         else:
             print('\033[1;31mA opção é inválida.\033[m')
             sleep(1)
 def configuracoes(email_logado): #Configurações para os clientes e entregadores
+    with open('nome.json', 'r', encoding='utf-8') as arq:
+        dados_json = json.load(arq)
+        info = dados_json.get(email_logado, {})
+        status_usuario = info.get('Status', 'Cliente')
+            
     opcao = ''
     while opcao != '4':
         valida.limpaTerminal()
@@ -167,13 +209,18 @@ def configuracoes(email_logado): #Configurações para os clientes e entregadore
         elif opcao == '4':
             print('Retornando ao Menu principal...')
             sleep(1)
-            MenuPrincipal(email_logado)
+            
+            if status_usuario == 'Entregador':
+                MenuPrincipalEntregador(email_logado) 
+            else:
+                MenuPrincipal(email_logado)
+                
             break
         else:
             print('\033[1;31mA opção é inválida.\033[m')
             sleep(1)
             
-def Compras(email_logado): 
+def Compras(email_logado): #Menu de compras
     opcao = ''
     while opcao != '3':
         valida.limpaTerminal()
